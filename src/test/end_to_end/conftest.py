cb1b647e2b2e38dbbc862683dbcd4d921a545524
Deifante Walters <dwalters@benevity.com>
2021-09-11 12:36:09 -0600

    Add a test for listing products.

--- src/test/end_to_end/conftest.py
+++ src/test/end_to_end/conftest.py
@@ -8,5 +8,9 @@ fake = faker.Faker()
 
 @pytest.fixture
 def new_random_product() -> dict:
-
     return {"name": fake.word(), "price": random.uniform(1, 100), "description": fake.sentence()}
+
+
+@pytest.fixture
+def products_route() -> str:
+    return "/api/products"
--- src/test/end_to_end/test_product.py
+++ src/test/end_to_end/test_product.py
@@ -1,3 +1,5 @@
+import pprint
+
 from fastapi.testclient import TestClient
 
 from manage_products.main import api
@@ -6,11 +8,26 @@ from manage_products.models.product_schemas import Product
 client = TestClient(api)
 
 
-def test_create_product(new_random_product: dict):
-    response = client.post("/api/products", json=new_random_product)
+def test_create_product(products_route: str, new_random_product: dict):
+    response = client.post(products_route, json=new_random_product)
     product_response = Product(**response.json())
     assert response.status_code == 201
     assert type(product_response) is Product
     assert product_response.price == new_random_product['price']
     assert product_response.name == new_random_product['name']
     assert product_response.description == new_random_product['description']
+    assert product_response.views == 0
+    assert product_response.is_deleted is False
+
+
+def test_list_products(products_route: str):
+    response = client.get(products_route)
+    products = [Product(**p) for p in response.json()]
+
+    for p in products:
+        assert type(p) is Product
+# def test_delete_product(products_route: str, new_random_product:dict):
+#     new_product = Product(**client.post(products_route, json=new_random_product).json())
+#
+#     delete_response = client.delete(products_route + new_product.id)
+#
